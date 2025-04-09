99b825b9166b25a7264f81e39cea6f795a2e4775
sosoye0n <sosoyeon98@naver.com>
2025-04-09 10:59:45 +0900

    [소연]스크립트 및 헤더 수정진행🫡

--- scripts/mypage.js
+++ scripts/mypage.js
@@ -1,72 +1,7 @@
-// const couponModalContainer = document.querySelector("#couponModal");
+// 헤더 요소
+const loginSuccesChange = document.querySelector(".loginSucces");
 
-// const couponModal = document.createElement("div");
-// couponModal.id = "couponModal";
-// couponModal.innerHTML = `
-//         <div class="coupon">
-//           <div class="modalHeader">
-//             <i class="fas fa-xmark close"></i>
-//             <div class="modalTitle">
-//               <p>보유쿠폰</p>
-//               <span>coupon</span>
-//             </div>
-//           </div>
-//           <ul class="couponContents">
-//             <li class="modalCouponInfoCon">
-//               <div class="modalCoupon">
-//                 <div class="couponBox1">
-//                   <div class="modalInfo">
-//                     <div class="membershipInfo">
-//                       <p>12<b>%</b></p>
-//                       <span>[프로모션] 스프링시즌세일</span>
-//                     </div>
-//                     <p class="subInfo">신규멤버, 첫구매 쿠폰 제공</p>
-//                   </div>
-//                 </div>
-//                 <div class="couponBox2">
-//                   <i class="fas fa-circle-check"></i>
-//                 </div>
-//               </div>
-//             </li>
-//             <li class="modalCouponInfoCon">
-//               <div class="modalCoupon">
-//                 <div class="couponBox1">
-//                   <div class="modalInfo">
-//                     <div class="membershipInfo">
-//                       <p>30<b>%</b></p>
-//                       <span>[이미스] 브랜드 중복쿠폰</span>
-//                     </div>
-//                     <p class="subInfo">50,000원 이상 구매시 사용 가능</p>
-//                   </div>
-//                 </div>
-//                 <div class="couponBox2">
-//                   <i class="fas fa-circle-check"></i>
-//                 </div>
-//               </div>
-//             </li>
-//             <li class="modalCouponInfoCon">
-//               <div class="modalCoupon">
-//                 <div class="couponBox1">
-//                   <div class="modalInfo">
-//                     <div class="membershipInfo">
-//                       <p><b>무료배송</b></p>
-//                       <span>[멤버십] 가입감사쿠폰</span>
-//                     </div>
-//                     <p class="subInfo">신규멤버십 가입 회원 대상</p>
-//                   </div>
-//                 </div>
-//                 <div class="couponBox2">
-//                   <i class="fas fa-circle-check"></i>
-//                 </div>
-//               </div>
-//             </li>
-//           </ul>
-//         </div>
-// `;
-
-// couponModalContainer.appendChild(couponModal);
-
-// console.log(couponModal);
+console.log(loginSuccesChange);
 
 // 모달 요소
 const couponModal = document.querySelector("#couponModal");
@@ -108,3 +43,12 @@ closeBtns.forEach((btn) => {
     }
   });
 });
+
+// 로그인 성공사 헤더 변경
+loginSuccesChange.innerHTML = `<span>logout</span><i class="fa-solid fa-right-from-bracket"></i>`;
+
+// function updateHeaderLoginState() {
+//   loginSuccesChange.innerHTML = "logout";
+//   loginSuccesChange.setAttribute("href", "/mypage.html");
+//   updateHeaderLoginState();
+// }
